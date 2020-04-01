c89500ce86e784a77315f2e825ee71fca53976f7
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-04-01 16:17:50 -0300

    Projeto finalizado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Calculator from './main/Calculator.jsx';
+import Calculator from './main/Calculator';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
